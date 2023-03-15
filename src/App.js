8aa8252ef8e6c7d288bd6dba2cdddb1d942ede50
Eliyas Kassaye <eliyasson11@gmail.com>
2023-03-15 15:11:06 +0200

    add id

--- src/App.js
+++ src/App.js
@@ -6,41 +6,78 @@ function App() { //This declares a functional component named App.
   //state variables using the useState hook:
   const [product, setProduct] = useState(""); //product: stores the current value of the input field for the product name.
   const [productsList, setProductsList] = useState([]); //store the list of products as an array
+  const [price, setPrice] = useState("");
+  
+  const [showDetails, setShowDetails] = useState();
+  const [cartItems, setCartItems] = useState([]);
 
   //Create a function to handle the button click event that will add the new product to the list:
   /*This function is called when the "Add Product" button is clicked. 
   It updates the productsList state variable by adding the current value of product to the end of the array.*/
   const handleAddProduct = () =>{
-    setProductsList([...productsList, product]);
+    const newProduct = {
+      id: Math.floor(Math.random() * 1000),
+      name: product,
+      price: price
+    };
+    setProductsList([...productsList, newProduct]);
     setProduct(""); //resets the product state variable to an empty string, which clears the input field.
+    setPrice("");
   }
 
   /*This function is called whenever the input field value changes. 
   It updates the product state variable with the new value entered by the user. */
   const updateChange =(e)=> {
-    setProduct(e.target.value);
+    if(e.target.name === "product"){
+      setProduct(e.target.value);
+    } else if (e.target.name === "price") {
+      setPrice(e.target.value);
+    } 
   }
 
-  const deleteButton = (item) => {
+  /* const deleteButton = (item) => {
 
     const newArray = productsList.filter((product) => product !== item);
     setProductsList(newArray);
+  } */
+
+  const detailsClick = (product) =>{
+      setShowDetails(product);
+      
   }
+
+  const handleAddToCartClick = (item) => {
+    setCartItems((prevCartItems) => [...prevCartItems, item]);
+    setShowDetails(false);
+  };
 
 
   return (
     <div className="App">
       <h3>Online Shopping Cart</h3>
-      <input type='text' value={product} onChange={updateChange}></input>
-      <br />
-      <button onClick={handleAddProduct}>Add Product</button>
+      <div>
+        <input type='text' name="product" value={product} onChange={updateChange}></input>
+        <input type='text' name="price" value={price} onChange={updateChange}></input>
+        <br />
+        <button onClick={handleAddProduct}>Add Product</button>
+      </div>
+      
       <ul>
         {productsList.map((product) =>{
-          return <li>{product}
-          <button onClick={() => deleteButton(product)}>x</button>
+          return <li>{product.name}
+          <button onClick={() => detailsClick(product)}>Details</button>
           </li>
         })}
       </ul>
+      {showDetails &&
+        <div>
+          <h4>Detail</h4>
+          <p>{showDetails.name}</p>
+          <p>{showDetails.price}€</p>
+          
+          <button onClick={() => setShowDetails(false)}>Close</button>
+        </div>
+      }
     </div>
   );
 }
