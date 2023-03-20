9739473d4e036611457eabbbc6c994c267072d20
Eliyas Kassaye <eliyasson11@gmail.com>
2023-03-20 20:18:01 +0200

    apply separation of concerns to addProduct component

--- src/AddProduct.js
+++ src/AddProduct.js
@@ -1,6 +1,17 @@
 import React, { useState, useEffect, } from 'react';
 
-function AddProduct({product, price, handleProductChange, handlePriceChange, handleAddProduct}) {
+function AddProduct(handleAddProduct) {
+    const [product, setProduct] = useState("");
+    const [price, setPrice] = useState("");
+
+    const handleProductChange = (e) => {
+        setProduct(e.target.value);
+      }
+    
+      const handlePriceChange = (e) => {
+        setPrice(e.target.value);
+      }
+
     return ( 
         <div>
             <h3 className="heading">Add Product</h3>
